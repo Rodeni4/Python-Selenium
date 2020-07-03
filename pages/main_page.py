661b93b1f4c6cc70c62584ec1bbd52faf7367722
Rodeni4 <rodenkosv@gmail.com>
2020-07-04 01:41:35 +0700

    Новый файл locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,12 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+#импортируйте новый класс с локаторами
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
     #модифицируем метод проверки ссылки на логин так, чтобы он выдавал адекватное сообщение об ошибке:
     def should_be_login_link(self):
-        #self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
