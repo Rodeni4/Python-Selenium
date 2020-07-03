5fd6e4d2425f6c2591fd60ef06d33ec0a1950ea0
Rodeni4 <rodenkosv@gmail.com>
2020-07-03 20:57:46 +0700

    Начало

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Сначала добавляем обработчик опции в функции pytest_addoption
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru, en, ...(etc.)")
+@pytest.fixture
+def browser(request):
+    # Добавим логику обработки командной строки. Для запроса значения параметра
+    user_language = request.config.getoption("language")
+    browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
+
+
+
