0959767e174b22270c5552ccc0f7b34b27ca73ea
Rodeni4 <rodenkosv@gmail.com>
2020-07-04 01:15:15 +0700

    Проверка элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+#Чтобы импортировать нужное нам исключение, в самом верху файла нужно указать:
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
@@ -7,3 +9,16 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+    #Добавим команду для неявного ожидания со значением по умолчанию в 10
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+    #Рализуем метод, в котором будем перехватывать исключение.
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+#Отлично! Теперь для всех проверок, что элемент действительно присутствует на странице, мы можем использовать этот метод.
