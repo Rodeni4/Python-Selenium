c56d677e75c347b24d36096020e895ad7269448d
Rodeni4 <rodenkosv@gmail.com>
2020-07-06 02:22:48 +0700

    Задание: добавление в корзину со страницы товара

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,13 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_REGISTER = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ADDED_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main>h1")
+    MESSAGE_COST_BASKET = (By.CSS_SELECTOR, "div.alert-info")
+    PRODUCT_COST = (By.CSS_SELECTOR, "p.price_color")
+
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,36 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def should_be_product_page(self):
+        self.should_be_product_page_url()
+        self.go_to_product_page()
+        self.solve_quiz_and_get_code()
+        self.should_message_added_basket()
+        self.should_message_product_cost()
+
+    def should_be_product_page_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "?promo=newYear" in self.browser.current_url, "'?promo=newYear' not in current url"
+
+    def go_to_product_page(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        button.click()
+
+    def should_message_added_basket(self):
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ADDED_BASKET)
+        assert "has been added to your basket" in message.text, "No message 'has been added to your basket'"
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        assert product_name.text in message.text, "Product name does not match"
+
+    def should_message_product_cost(self):
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_COST_BASKET)
+        assert "Your basket total is now" in message.text, "No message 'Your basket total is now'"
+        product_cost = self.browser.find_element(*ProductPageLocators.PRODUCT_COST)
+        assert product_cost.text in message.text, "Basket price doesn't match"
+
+
+
+
+
